3078d378ed46ce9d49c0ffe283390ac36a71210d
Ritesh Keloth <30933593+riteshjkeloth@users.noreply.github.com>
2022-02-21 13:57:10 +0100

    Add signature verification !

--- branch-protection.py
+++ branch-protection.py
@@ -1,3 +1,4 @@
+import hashlib
 import hmac
 import os
 import requests
@@ -10,8 +11,8 @@ app = Flask(__name__)
 
 @app.route("/listener/repo-event", methods=['POST'])
 def repo_event_listener():
-    # if verify_signature(request) != "OK":
-    #     raise Exception("X-Hub-Signature-256 does not match!!")
+    if verify_signature(request) != "OK":
+        return '{"message":"X-Hub-Signature-256 does not match!!"}', 400
     request_body = request.json
     if request_body['action'] == "created":
         url = request_body['repository']['url']
@@ -20,9 +21,9 @@ def repo_event_listener():
         user = request_body['sender']['login']
         branch = "main"
         if set_branch_protection(url, headers, branch) != "OK":
-            raise Exception("Exception while setting branch protection !")
+            return '{"message":"Exception while setting branch protection !"}', 500
         if create_issue(url, headers, user) != "OK":
-            raise Exception("Exception while creating issue !")
+            return '{"message": "Branch protection rule created, however failed while creating issue !"}', 500
     return "OK"
 
 
@@ -31,12 +32,12 @@ def set_branch_protection(url, headers, branch):
     url = url + "/branches/" + branch + "/protection"
     r = requests.put(url, data=open(path, 'rb'), headers=headers)
     if r.status_code == 200:
-        print ("Branch protection rule created !")
+        print ("Branch protection rule created(updates it if already existing) !")
     return "OK"
 
 
 def create_issue(url, headers, user):
-    issue_json = '{"title":"Branch protection rule created", "body":"@' + user + ' A new branch protection rule was added in this repo"}'
+    issue_json = '{"title":"Branch protection rule created/updated", "body":"@' + user + ' A new branch protection rule was added(updates it if already existing) in this repo"}'
     url = url + "/issues"
     r = requests.post(url, issue_json, headers=headers)
     if r.status_code == 201:
@@ -47,13 +48,13 @@ def create_issue(url, headers, user):
 def verify_signature(req):
     signature = req.headers['X-Hub-Signature-256']
     local_key = os.environ.get("GITHUB_WEBHOOK_TOKEN")
+    # local_key = os.environ.get("GITHUB_WEBHOOK_TOKEN_INVALID") # for testing
     hashname, hashval = signature.split("=")
     digest = hmac.new(
-        local_key.encode("utf-8"),
-        msg=None,
-        digestmod=hashname
+        key=local_key,
+        msg=req.get_data(),
+        digestmod=hashlib.sha256
     ).hexdigest()
-    # digest = hmac.new('123'.encode(), 'asdfasf'.encode(), 'sha256').hexdigest()
-    if hmac.compare_digest(digest.encode(), hashval.encode("utf-8")):
+    if hmac.compare_digest(digest.encode(), hashval.encode()):
         return "OK"
 
